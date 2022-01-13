aaa7e25614b9d0d2f8d2c2422a13ac7fe4145a2f
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2022-01-13 16:35:42 -0300

    first commit :tada:

---
+++ android/app/src/main/kotlin/com/example/primeiro_camp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.primeiro_camp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Center(
+      child: Text(
+        'Ola mundo',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  ));
+}
